27fac53cca62fd6f06c4c3516abc33887dd33d53
hman09 <14950@coderacademy.edu.au>
2023-10-25 20:11:53 +1100

    room template started. need to settle on controls/tutorial before finalising room

--- src/main.py
+++ src/main.py
@@ -5,25 +5,35 @@ import datetime
 # make class for user, room and inventory
 # game will need to be in loop with the final stage closing the loop and ending the game
 # add end of game message outside of above mentioned loop
+# make r1 loop
 
 def tut():
     while user.first_try == 'Y':
         print('Blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah')
         user.first_try = 'N'
 
-
+def room():
+    while r1.lock == 'locked':
+        input()
+        r1.lock = 'unlocked'
 
 
 
 # Main
-username = input('What is your name? ')
-tutorial = input('Do you want the tutorial?(Y/N) ')
+username = 'H'#input('What is your name? ')
+tutorial = 'N'#input('Do you want the tutorial?(Y/N) ')
 
 user = txtadv.Player(username, tutorial, 0)
 
+r1 = txtadv.Rooms('locked', 'key')
+    
+
 tut()
+room()
+print(r1.lock)
 
-
+# while user.levels_complete == 0:
+    
 
 
 
