21530eca9dbf55faa7616b896078b93ead38bbc9
hman09 <14950@coderacademy.edu.au>
2023-10-25 17:00:45 +1100

    made class for player and lets user input their name

---
+++ src/main.py
@@ -0,0 +1,7 @@
+import txtadv
+
+username = input('What is your name? ')
+
+name = txtadv.Player(username)
+
+print(f'your name is. {name.name}.')
---
+++ src/txtadv.py
@@ -0,0 +1,8 @@
+class Player:
+    def __init__(self, name):
+        self.name = name
+        # self.inv = Invertory()
+
+class Invertory:
+    def __init__(self, items):
+        self.items = items
