e5b9bf445caf8bad06e039059427579d2e326deb
hman09 <14950@coderacademy.edu.au>
2023-10-25 18:15:07 +1100

    games base started. name input done, tutorial opt done and exit message done

--- src/main.py
+++ src/main.py
@@ -1,7 +1,35 @@
 import txtadv
+import datetime
 
+# let user be able to set name
+# make class for user, room and inventory
+# game will need to be in loop with the final stage closing the loop and ending the game
+# add end of game message outside of above mentioned loop
+
+def tut():
+    while user.first_try == 'Y':
+        print('Blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah')
+        user.first_try = 'N'
+
+
+
+
+
+# Main
 username = input('What is your name? ')
+tutorial = input('Do you want the tutorial?(Y/N) ')
 
-name = txtadv.Player(username)
+user = txtadv.Player(username, tutorial, 0)
 
-print(f'your name is. {name.name}.')
+tut()
+
+
+
+
+
+
+
+# put everything above here and uncomment at the end
+# current_datetime = datetime.datetime.now()
+# print(f'Congratulations {user.name} you finished the game!!', current_datetime)
+
--- src/txtadv.py
+++ src/txtadv.py
@@ -1,8 +1,20 @@
 class Player:
-    def __init__(self, name):
+    def __init__(self, name, first_try, levels_complete):
         self.name = name
-        # self.inv = Invertory()
+        self.inv = Inventory([])
+        self.first_try = first_try
+        self.levels_complete = levels_complete
 
-class Invertory:
+class Inventory:
     def __init__(self, items):
         self.items = items
+
+class Rooms:   
+    def __init__(self, lock, condition):
+        self.lock = lock
+        self.condition = condition
+        self.inv = Inventory([])
+
+class Help:
+    def __init__(self):
+        pass
